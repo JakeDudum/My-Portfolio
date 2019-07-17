2a987c10406e21f5eb396c76a3df7ffda926c4d6
Jake Dudum <jakedudum@gmail.com>
2019-07-17 12:44:37 -0700

    Updated navbar to navigate to sections on the page using app.js

---
+++ assets/javascript/app.js
@@ -0,0 +1,19 @@
+(function ($) {
+    $(function () {
+
+        $('.mainnav').sidenav();
+
+    }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+// Scrolls the page to the section clicked on in navbar
+$(function () {
+    $('ul.nav a').bind('click', function (event) {
+        event.preventDefault();
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1000);
+        event.preventDefault();
+    });
+});
